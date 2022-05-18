6c8fc1c324e04a0609b2fd62c1afdf3cf3428134
LunaGen <lugeng2019@gmail.com>
2022-05-17 22:55:33 -0700

    tested submit button
    submit button works, and also the required phone No. input has been fixed

--- src/components/NewPackagePage.js
+++ src/components/NewPackagePage.js
@@ -7,17 +7,49 @@ import {
   Button,
   Divider,
   DatePicker,
+  message,
 } from 'antd';
  
  
 class NewPackagePage extends React.Component {
+  formRef = React.createRef();
+    state = {
+      authed: false,
+      loading: false,
+    };
+
+    handleSubmit = async () => {
+      const formInstance = this.formRef.current;
+   
+      try {
+        await formInstance.validateFields();
+      } catch (error) {
+        return;
+      }
+   
+      this.setState({
+        loading: true,
+      });
+   
+      try {
+        //await submit(formInstance.getFieldsValue(true), this.state.authed);
+        message.success("Submit Successfully");
+      } catch (error) {
+        message.error(error.message);
+      } finally {
+        this.setState({
+          loading: false,
+        });
+      }
+    };
+ 
+ 
   render() {
     const layout = {
       labelCol: { span: 8 },
       wrapperCol: { span: 16 },
     };
     
-    /* eslint-disable no-template-curly-in-string */
     const validateMessages = {
       required: '${label} is required!',
       types: {
@@ -28,7 +60,6 @@ class NewPackagePage extends React.Component {
         range: '${label} must be between ${min} and ${max}',
       },
     };
-    /* eslint-enable no-template-curly-in-string */
     
     const onFinish = () => {
       console.log("submitted");
@@ -40,21 +71,24 @@ class NewPackagePage extends React.Component {
       console.log(date, dateString);
     }
 
+    const prefixSelector = (
+      <Form.Item name='prefix' noStyle>
+        <Select style={{ width: 90 }}>
+          <Option value="1">+1</Option>
+          <Option value="86">+86</Option>
+        </Select>
+      </Form.Item>
+    );
+
     return (
-        <Form {...layout} className="OrderInfo" onFinish={onFinish} validateMessages={validateMessages} labelCol={{ span: 7 }} wrapperCol={{ span: 8 }}>
+        <Form {...layout} className="OrderInfo" ref={this.formRef} onFinish={this.onFinish} validateMessages={validateMessages} labelCol={{ span: 7 }} wrapperCol={{ span: 8 }}>
           <div>Sender</div>
           <Divider />
           <Form.Item name={['sender', 'name']} label="Name" rules={[{ required: true }]}>
               <Input/>    
           </Form.Item>
-          <Form.Item name={['sender', 'phone']} label="Phone" rules={[{ required: true }]}>
-          <Input.Group compact >
-              <Select style={{ width: '25%' }}>
-                <Option value="1">+1</Option>
-                <Option value="86">+86</Option>
-              </Select>
-              <Input style={{ width: '75%' }}/>
-              </Input.Group>
+          <Form.Item name={['sender', 'phone']}  label="Phone" rules={[{ required: true }]}>
+                <Input addonBefore={prefixSelector} style={{ width: '100%' }}/>
           </Form.Item>
           <Form.Item name={['sender', 'address']} label="Address" rules={[{ required: true }]}>
               <Input/>
@@ -63,17 +97,21 @@ class NewPackagePage extends React.Component {
 
           <p>Recipient</p>
           <Divider />
-          <Form.Item name={['recipient', 'name']} label="Name" >
+          <Form.Item name={['recipient', 'name']} label="Name" rules={[{ required: true }]}>
             <Input />
           </Form.Item>
           <Form.Item name={['recipient', 'phone']} label="Phone" >
             <Input.Group compact >
-              <Select style={{ width: '25%' }}>
-                <Option value="1">+1</Option>
-                <Option value="86">+86</Option>
-              </Select>
-              <Input style={{ width: '75%' }}/>
-              </Input.Group>
+              <Form.Item noStyle name={['recipient','prefix']}>
+                  <Select style={{ width: '15%' }}>
+                    <Option value="1">+1</Option>
+                    <Option value="86">+86</Option>
+                  </Select>
+                </Form.Item>
+                <Form.Item noStyle name={['recipient','phoneNo']}>
+                  <Input style={{ width: '85%' }}/>
+                </Form.Item>
+            </Input.Group>
           </Form.Item>
           <Form.Item name={['recipient', 'address']} label="Address" rules={[{ required: true }]}>
             <Input />
@@ -98,7 +136,7 @@ class NewPackagePage extends React.Component {
           </Form.Item>
 
           <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-            <Button type="primary" htmlType="submit">
+            <Button type="primary" onClick={this.handleSubmit} disabled={this.state.loading} >
               Submit
             </Button>
           </Form.Item>
